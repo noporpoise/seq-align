d9af8121156c03f0371cca0be988e3af7aa07756
Isaac Turner <turner.isaac@gmail.com>
2013-06-12 17:42:59 +0100

    Fix sort_r again

--- src/sort_r.c
+++ src/sort_r.c
@@ -1,4 +1,5 @@
 
+#include <stdlib.h>
 #include "sort_r.h"
 
 #if defined(__MINGW32__) || defined(__OpenBSD__) || defined(AMIGA) || \
@@ -32,6 +33,15 @@ int sort_r_arg_swap(void *s, const void *aa, const void *bb)
   struct sort_r_data *ss = (struct sort_r_data*)s;
   return (ss->compar)(aa, bb, ss->arg);
 }
+
+#if (defined _GNU_SOURCE || defined __GNU__ || defined __linux__)
+
+typedef int(* __compar_d_fn_t)(const void *, const void *, void *);
+extern void qsort_r (void *__base, size_t __nmemb, size_t __size,
+                     __compar_d_fn_t __compar, void *__arg)
+  __nonnull ((1, 4));
+
+#endif
 
 void sort_r(void *base, size_t nel, size_t width,
             int (*compar)(const void *a1, const void *a2, void *aarg), void *arg)
