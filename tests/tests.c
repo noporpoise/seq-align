857d3d5e5d058773048e04bcd77d902d69d855a0
andrea <a.campagner@campus.unimib.it>
2015-01-31 10:40:52 -0800

    Writing tests

--- tests/tests.c
+++ tests/tests.c
@@ -1,5 +1,6 @@
 #include "cutest.h"
 #include "needleman_wunsch.h"
+#include "smith_waterman.h"
 #include <string.h>
 
 //No gap is expected in the longer sequence
@@ -50,7 +51,9 @@ void test_no_gaps_in_longer(void)
   TEST_CHECK(strcmp(result->result_a, "actcg") == 0 && strcmp(result->result_b, "ggctc")== 0);
 }
 
-void test_gaps_only_at_ends_in1(void)
+/* If sequence with only gaps at ends allowed is the shorter, then the expected result is the
+ * same as nogaps */
+void test_gaps_only_at_ends_in_shorter(void)
 {
   nw_aligner_t *nw = needleman_wunsch_new();
   alignment_t *result = alignment_create(256);
@@ -58,7 +61,6 @@ void test_gaps_only_at_ends_in1(void)
   const char* seq_a = "acgtc";
   const char* seq_b = "aaaaacgc"; 
 
-  // Decide on scoring
   int match = 1;
   int mismatch = -2;
   int gap_open = -4;
@@ -71,22 +73,64 @@ void test_gaps_only_at_ends_in1(void)
                no_gaps_in_a, no_gaps_in_b, gaps_ends_a, 0, 0, 0);
                
   needleman_wunsch_align(seq_a, seq_b, &scoring, nw, result);
-  TEST_CHECK(strcmp(result->result_b, "aaaaacg-c") == 0 && strcmp(result->result_a, "----acgtc")== 0);
-  seq_b = "aaaaacgct";
-  needleman_wunsch_align(seq_a, seq_b, &scoring, nw, result);
-  TEST_CHECK(strcmp(result->result_b, "aaaaacgct") == 0 && strcmp(result->result_a, "----acgtc")== 0);
-  mismatch = -10;
+  TEST_CHECK(strcmp(result->result_a, "----acgtc") == 0 && strcmp(result->result_b, "aaaaacg-c")== 0);
+}
+
+/* If sequence with only gaps at ends allowed is the longer, then the expected result is not
+ * the same as nogaps because gaps are allowed at ends of the longest sequence*/
+void test_gaps_only_at_ends_in_longer(void)
+{
+  nw_aligner_t *nw = needleman_wunsch_new();
+  alignment_t *result = alignment_create(256);
+  
+  const char* seq_a = "acgct";
+  const char* seq_b = "aaaaacgc"; 
+
+  int match = 1;
+  int mismatch = -2;
+  int gap_open = -4;
+  int gap_extend = -1;
+  
+  char gaps_ends_b = 1, no_gaps_in_a = 0, no_gaps_in_b = 0;
+  scoring_t scoring;
   scoring_init(&scoring, match, mismatch, gap_open, gap_extend,
                0, 0,
-               no_gaps_in_a, no_gaps_in_b, gaps_ends_a, 0, 0, 0);
+               no_gaps_in_a, no_gaps_in_b, 0, gaps_ends_b, 0, 0);
                
   needleman_wunsch_align(seq_a, seq_b, &scoring, nw, result);
-  TEST_CHECK(strcmp(result->result_b, "aaaaacg-ct") == 0 && strcmp(result->result_a, "----acgtc-")== 0);
+  TEST_CHECK(strcmp(result->result_a, "a----cgct") == 0 && strcmp(result->result_b, "aaaaacgc-")== 0);
+}
+
+void test_no_gaps_smith_waterman(void)
+{
+  sw_aligner_t *sw = smith_waterman_new();
+  alignment_t *result = alignment_create(256);
+
+  const char* seq_a = "gacag";
+  const char* seq_b = "tgaagt"; 
+  
+  int match = 1;
+  int mismatch = -2;
+  int gap_open = -4;
+  int gap_extend = -1;
+  
+  char no_gaps_in_a = 1, no_gaps_in_b = 1;
+  scoring_t scoring;
+  scoring_init(&scoring, match, mismatch, gap_open, gap_extend,
+               0, 0,
+               no_gaps_in_a, no_gaps_in_b, 0, 0, 0, 0);
+  smith_waterman_align(seq_a, seq_b, &scoring, sw);
+  smith_waterman_fetch(sw, result);
+  TEST_CHECK(strcmp(result->result_a, "ga") == 0 && strcmp(result->result_b, "ga") == 0);
+  smith_waterman_fetch(sw, result);
+  TEST_CHECK(strcmp(result->result_a, "ag") == 0 && strcmp(result->result_b, "ag") == 0);
 }
 
 TEST_LIST = {
   { "no_gaps_in_longer", test_no_gaps_in_longer },
   { "no_gaps_equal_length", test_no_gaps_equal},
-  { "gaps_only_at_ends_in_shorter", test_gaps_only_at_ends_in1},
+  { "gaps_only_at_ends_in_shorter", test_gaps_only_at_ends_in_shorter},
+  { "gaps_only_at_ends_in_longer", test_gaps_only_at_ends_in_longer},
+  { "no_gaps_smith_waterman", test_no_gaps_smith_waterman},
   { 0 }
 };
